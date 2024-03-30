90708a720b5c3a0d4982a42579d543a2d74fbb37
nimaMakhmali <nmkhmly790@gmail.com>
2024-03-30 15:57:09 +0330

    add file

---
+++ travelManagement.cpp
@@ -0,0 +1,27 @@
+//***********************************************************
+//    Nima Makhmali -------> Buali sina university of IRAN❤️
+//***********************************************************
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <windows.h>
+
+using namespace std;
+
+class
+{};
+
+class
+{};
+
+class
+{};
+
+class 
+{};
+
+//******************************MAIN********************************
+main()
+{
+  return 0;
+}
